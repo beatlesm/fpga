4a66a9af73e5c2dc7a9eaf72848be85904c2d12b
Rohit Dilip <rdilip@nat-oitwireless-inside-vapornet100-d-25990.Princeton.EDU>
2017-04-04 20:49:46 -0400

    Added signal.v

---
+++ usrp3/top/b200/signal.v
@@ -0,0 +1,12 @@
+module signal(radio_clk, sig_out);
+	input radio_clk;
+	output reg sig_out;
+	reg [1:0] counter;
+
+	always @(posedge radio_clk)
+		begin
+			counter = counter + 1'b1;
+			sig_out = counter[1];
+		end
+endmodule
+

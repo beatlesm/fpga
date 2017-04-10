492c15f39f8a87fe3fd0e6a70ac9475f7ed2c027
thompsonlab <thompsonlab@thompsonlab3.ee.Princeton.EDU>
2017-04-10 10:52:26 -0400

    Slight syntax error

--- usrp3/lib/radio_200/input_signal.v
+++ usrp3/lib/radio_200/input_signal.v
@@ -11,8 +11,8 @@ module input_signal(radio_clk, get_tx);
       	tx_out[15:0] <= 16'b1010101010101010;
 	end 
       else begin
-      	tx[31:16] <= 16'b0000000000000000;
-      	tx[15:0] <= 16'b0000000000000000;
+      	tx_out[31:16] <= 16'b0000000000000000;
+      	tx_out[15:0] <= 16'b0000000000000000;
 	end
    end
    assign get_tx = tx_out;
